58ec24ffffe5ea3e859d690283f60b19293167b0
Praveen795 <93025027+Praveen795@users.noreply.github.com>
2021-11-10 09:05:22 +0530

    Add files via upload

---
+++ coding.java
@@ -0,0 +1,7 @@
+package coding
+public class coding {
+	public static void main(String[] args) {
+		System.out.print("hello world");
+	}
+
+}
---
+++ forloop.java
@@ -0,0 +1,10 @@
+package forloop;
+
+public class forloop {
+	public static void main(String[] args) {
+		for(int i=10; i<=1;i--) {
+			System.out.println(i*2);
+		}
+	}
+
+}
